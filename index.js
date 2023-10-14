cc56eebc934507508114f2db6689d9a69469380a
Maxwell <njoromax5@gmail.com>
2023-10-14 20:47:38 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+// const testVar = {}
+
+// function testFunc() {
+//   return "hi"
+// }
+
+const h1 = document.createElement ("h1")
+h1.innerHTML = "My HTML adventure";
+document.body.appendChild(h1)
